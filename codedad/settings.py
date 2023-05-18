e3e478bf14aa414174e3f3e57cd8297c02307b95
EndaleKinfe <endlakinfe5@gmail.com>
2023-05-18 12:51:11 +0300

    app is not working cause craigslist changed their responseform try to fix it another time

--- codedad/settings.py
+++ codedad/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os.path
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
